58d12e3b626eabe3827dfac666d8d75cd36da3b3
Andik Kurniawan <andikk362@gmail.com>
2022-12-22 07:41:05 +0700

    exclude route login-process from csrf protection

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,7 @@
 	<link rel="stylesheet" type="text/css" href="/login_template/css/main.css">
 </head>
 <body>
-	
+
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
@@ -24,7 +24,6 @@
 				</div>
 
 				<form class="login100-form validate-form" method="POST" action="{{ route('login-process') }}">
-                    @csrf
 
 					<span class="login100-form-title">
 						Login
@@ -39,6 +38,7 @@
                     @endif
 
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                        @csrf
 						<input class="input100" type="text" name="username" placeholder="Masukkan username">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
@@ -53,7 +53,7 @@
 							<i class="fa fa-key" aria-hidden="true"></i>
 						</span>
 					</div>
-					
+
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn" type="submit">
 							Login
@@ -72,7 +72,7 @@
 			</div>
 		</div>
 	</div>
-	
+
 	<script src="/login_template/vendor/jquery/jquery-3.2.1.min.js"></script>
 	<script src="/login_template/vendor/bootstrap/js/popper.js"></script>
 	<script src="/login_template/vendor/bootstrap/js/bootstrap.min.js"></script>
